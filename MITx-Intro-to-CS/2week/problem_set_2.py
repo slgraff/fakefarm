31e78531d6ec779493e31a723fe3d8822f46fb74
Dave Woodall <dmwoodall@gmail.com>
2012-10-16 10:50:36 -0600

    cleaning up file, moving problem to top

--- MITx-Intro-to-CS/2week/problem_set_2.py
+++ MITx-Intro-to-CS/2week/problem_set_2.py
@@ -20,43 +20,6 @@ upper_bound = ( balance * ( 1 + monthly_interest ) ** 12 ) / 12 )
 # Month 2
 # b2 = ( b1 - p1 ) * ( 1 + r/12 )
 
-# PROBLEM 1
-# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
-
-# For each month, calculate statements on the monthly payment and remaining balance, and print to screen something of the format:
-# Month: 1
-# Minimum monthly payment: 96.0
-# Remaining balance: 4784.0
-
-# A summary of the required math is found below:
-
-# Monthly interest rate= (Annual interest rate) / 12
-# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-# Updated balance each month = (Previous balance - Minimum monthly payment) x (1 + Monthly interest rate)
-
-# def oneYear(balance, annualInterestRate, monthlyPaymentRate):
-#   new_balance = balance
-#   my_payment = monthlyPaymentRate * new_balance
-#   total_paid = my_payment
-#   month = 0
-
-#   while month < 12:
-#     month += 1
-#     new_balance  = (new_balance - my_payment) * (1 +  (annualInterestRate / 12))
-#     total_paid += my_payment
-#     print "Month: " + str(month)
-#     print "Minimum monthly payment: " + str(round(my_payment,2))
-#     print "Remaining balance: " + str(round(new_balance,2))
-
-#     my_payment = monthlyPaymentRate * new_balance
-
-#   print "Total paid: " + str(round(total_paid,2))
-#   print "Remaining balance: " + str(round(new_balance,2))
-
-# # Calling the method
-# oneYear(4213,0.2,0.04)
-# oneYear(4842, 0.2, 0.04)
-
 # PROBLEM 2
 
 # Write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months. By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be paid each month.
@@ -176,26 +139,40 @@ upper_bound = ( balance * ( 1 + monthly_interest ) ** 12 ) / 12 )
 #     print "Amount paid: " + str(my_payment)
 #     print "Balance: " + str(new_balance)
 
+# PROBLEM 1
+# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
 
+# For each month, calculate statements on the monthly payment and remaining balance, and print to screen something of the format:
+# Month: 1
+# Minimum monthly payment: 96.0
+# Remaining balance: 4784.0
 
+# A summary of the required math is found below:
 
+# Monthly interest rate= (Annual interest rate) / 12
+# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+# Updated balance each month = (Previous balance - Minimum monthly payment) x (1 + Monthly interest rate)
 
+# def oneYear(balance, annualInterestRate, monthlyPaymentRate):
+#   new_balance = balance
+#   my_payment = monthlyPaymentRate * new_balance
+#   total_paid = my_payment
+#   month = 0
 
+#   while month < 12:
+#     month += 1
+#     new_balance  = (new_balance - my_payment) * (1 +  (annualInterestRate / 12))
+#     total_paid += my_payment
+#     print "Month: " + str(month)
+#     print "Minimum monthly payment: " + str(round(my_payment,2))
+#     print "Remaining balance: " + str(round(new_balance,2))
 
+#     my_payment = monthlyPaymentRate * new_balance
 
+#   print "Total paid: " + str(round(total_paid,2))
+#   print "Remaining balance: " + str(round(new_balance,2))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # Calling the method
+# oneYear(4213,0.2,0.04)
+# oneYear(4842, 0.2, 0.04)
 
