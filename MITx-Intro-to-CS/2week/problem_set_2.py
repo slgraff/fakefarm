97e8875bfecbe5ce34e5c1f4678b1f93f9d7fdde
Dave Woodall <dmwoodall@gmail.com>
2012-10-16 10:49:49 -0600

    cleaning up file, moving problem to top

--- MITx-Intro-to-CS/2week/problem_set_2.py
+++ MITx-Intro-to-CS/2week/problem_set_2.py
@@ -1,6 +1,18 @@
+# PROBLEM 3
+
+balance = 0
+annualInterestRate = 0.2
+
+new_balance = balance
+monthly_interest = ( annualInterestRate / 12 )
+payment = 10
+step = 0.01
+
+lower_bound = balance / 12
+upper_bound = ( balance * ( 1 + monthly_interest ) ** 12 ) / 12 )
+
+
 # FORMULAS
-
-
 
 # Month 1
 # b1 = ( b0 - p0 ) * ( 1 + r/12 )
@@ -49,7 +61,6 @@
 
 # Write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months. By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be paid each month.
 
-
 ### ---------------
 ### Version 4
 ### --------------
@@ -81,22 +92,12 @@
 # if not, then I will increase the amount to pay
 # and try to retai
 
-
-
-
-
-
-
-
-
-
 # annualInterestRate = 0.2
 
 # balance = 4773
 # new_balance = balance
 
 # minPay = 10
-
 
 # while balance > 0:
 #   for month in range(1,13):
@@ -111,17 +112,9 @@
 
 # print "Lowest Payment: " + str(minPay)
 
-
-
-
-
-
-
-
 # -----------------------------------------------
 # Version 3
 # ---------------------
-
 
 # month = 0
 # balance = 5000
@@ -183,17 +176,6 @@
 #     print "Amount paid: " + str(my_payment)
 #     print "Balance: " + str(new_balance)
 
-# PROBLEM 3
-
-balance = 0
-new_balance = balance
-annualInterestRate = 0.2
-monthly_interest = ( annualInterestRate / 12 )
-payment = 10
-step = 0.01
-
-lower_bound = balance / 12
-upper_bound = ( balance * ( 1 + monthly_interest ) ** 12 ) / 12 )
 
 
 
