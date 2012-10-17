ecaa1554fe8ebc8166911218e6f7ea6dce4ff2b7
Dave Woodall <dmwoodall@gmail.com>
2012-10-17 11:21:19 -0600

    Maybe I'll learn some C

---
+++ Learn_C/ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+  puts("Hello world.");
+  puts("Exploring anything and everthing");
+  puts("I notice that C uses semi-colons, just like javascript");
+  return 0;
+}
