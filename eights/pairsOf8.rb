8927a7fda941dd2eeb93409dc861927457bf38bb
Dave Woodall <dmwoodall@gmail.com>
2012-11-10 17:03:59 -0700

    add turn counter to show number of iterations to process output

--- eights/pairsOf8.rb
+++ eights/pairsOf8.rb
@@ -4,9 +4,11 @@
 def combinations_of_eight(list_of_numbers)
 
   matches = []
-
+  turns = 0
   list_of_numbers.each do |first_number|
+    turns += 1
     list_of_numbers.each do |second_number|
+      turns += 1
       if list_of_numbers[first_number] != list_of_numbers[second_number]
         if first_number + second_number == 8
           match = []
@@ -17,9 +19,11 @@ def combinations_of_eight(list_of_numbers)
     end
   end
 
-  if list_of_numbers.count(4) == 2
-    matches << [4,4]
-  end
-
+  matches << [4,4] if list_of_numbers.count(4) == 2
+  puts "total turns: #{turns}"
   matches = matches.uniq
 end
+
+list = [1,1,2,3,4,5,6,7,8,19,0,1,11,2,3,4,5,36,7,8,9,0,21,1,2,3,4,15,6,7,8,9,0]
+
+puts combinations_of_eight(list)
