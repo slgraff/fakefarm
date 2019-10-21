dd4b36c9feedadf77e54a42256bf373f64883b2e
David Woodall <woodallD@aetna.com>
2019-10-21 15:44:50 -0600

    ruby exceptions practice

--- 2019/ruby_exceptions/init.rb
+++ 2019/ruby_exceptions/init.rb
@@ -4,11 +4,22 @@ class Fruit
   end
 
   def eat
-    raise StandardError, @type
+    if @type == 'apple'
+      raise FruitIsDead.new('boom is my message that I raised')
+    else
+      puts 'yummy'
+    end
   rescue => e
-    require 'pry'; binding.pry
+    puts e.class.name
+    puts e.message
+    puts e.backtrace
   end
+
+  class FruitIsDead < StandardError; end
 end
+
+banana = Fruit.new('banana')
+banana.eat
 
 apple = Fruit.new('apple')
 apple.eat
