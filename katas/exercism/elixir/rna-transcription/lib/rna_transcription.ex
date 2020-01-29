cbb941f38eca66f299acd931de5a67f516b15fd7
Dave Woodall <hay@fakefarm.com>
2020-01-29 13:11:44 -0700

    Refator RNA

--- katas/exercism/elixir/rna-transcription/lib/rna_transcription.ex
+++ katas/exercism/elixir/rna-transcription/lib/rna_transcription.ex
@@ -9,32 +9,15 @@ defmodule RnaTranscription do
   """
 
   def to_rna(char) do
-    stringify_codepoint(char)
-    |> make_conversion
-    |> codepoint_string
+    Enum.map(char, &(convert(&1)))
   end
 
-  def stringify_codepoint(char) do
-    List.to_string(char)
-    |> String.split("")
-    |> Enum.reject( &(&1 == "") )
-  end
-
-  def make_conversion(code) do
-    Enum.map(code, fn(c) -> convert(c) end)
-    |> List.to_string
-  end
-
-  def convert(head) do
+  defp convert(head) do
     case head do
-      "G" -> "C"
-      "C" -> "G"
-      "T" -> "A"
-      "A" -> "U"
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
     end
-  end
-
-  def codepoint_string(char) do
-    String.to_charlist(char)
   end
 end
