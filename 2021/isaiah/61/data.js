fa4438caca54923a34c1d6f92010774c02ab47cc
Dave Woodall <dave@dime.is>
2021-08-06 10:26:02 -0400

    Add Isiah 61

---
+++ 2021/isaiah/61/data.js
@@ -0,0 +1,46 @@
+[
+  {
+    "no": 1,
+    "text": "The Spirit of YHVH Elohim is upon me, because YHVH has anointed me to bring good news to the poor; YHVH Elohim has sent me to bind up the brokenhearted, to proclaim liberty to the captives, and the opening of the prison to those who are bound;"
+  },
+  {
+    "no": 2,
+    "text": "to proclaim the year of YHVH’s favor, and the day of vengeance of our Elohim; to comfort all who mourn;"
+  },
+  {
+    "no": 3,
+    "text": "to grant to those who mourn in Zion— to give them a beautiful headdress instead of ashes, the oil of gladness instead of mourning, the garment of praise instead of a faint spirit; that they may be called oaks of righteousness, the planting of YHVH, that YHVH Elohim may be glorified."
+  },
+  {
+    "no": 4,
+    "text": "They shall build up the ancient ruins; they shall raise up the former devastations; they shall repair the ruined cities, the devastations of many generations."
+  },
+  {
+    "no": 5,
+    "text": "Strangers shall stand and tend your flocks; foreigners shall be your plowmen and vinedressers;"
+  },
+  {
+    "no": 6,
+    "text": "but you shall be called the priests of YHVH; they shall speak of you as the ministers of our Elohim; you shall eat the wealth of the nations, and in their glory you shall boast."
+  },
+  {
+    "no": 7,
+    "text": "Instead of your shame there shall be a double portion; instead of dishonor they shall rejoice in their lot; therefore in their land they shall possess a double portion; they shall have everlasting joy."
+  },
+  {
+    "no": 8,
+    "text": "For I YHVH love justice; I YHVH hate robbery and wrong; I YHVH will faithfully give them their recompense, and I YHVH will make an everlasting covenant with them."
+  },
+  {
+    "no": 9,
+    "text": "Their offspring shall be known among the nations, and their descendants in the midst of the peoples; all who see them shall acknowledge them, that they are an offspring YHVH has blessed."
+  },
+  {
+    "no": 10,
+    "text": "I will greatly rejoice in YHVH; my soul shall exult in my Elohim, for YHVH Elohim has clothed me with the garments of salvation; YHVH Elohim has covered me with the robe of righteousness, as a bridegroom decks himself like a priest with a beautiful headdress, and as a bride adorns herself with her jewels."
+  },
+  {
+    "no": 11,
+    "text": "For as the earth brings forth its sprouts, and as a garden causes what is sown in it to sprout up, so YHVH Elohim will cause righteousness and praise to sprout up before all the nations."
+  }
+]
---
+++ 2021/isaiah/61/script.js
@@ -0,0 +1,19 @@
+function boom(chapter) {
+  fetch(`http://localhost:8000/isaiah/${chapter}/data.js`)
+    .then(response => response.json())
+    .then(data => {
+      return data
+    });
+}
+
+function title(content) {
+  return ejs.render('<h1><%= title %></h1>', {title: content});
+}
+
+function body(content) {
+ return ejs.render('<p><%= title %></p>', {title: content});
+}
+
+
+let b = boom(61);
+debugger
